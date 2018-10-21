d1d4a1037a072d32ef317eb5004d8c26e6a5ddb4
SIJIA FEI <fsj0716@hotmail.com>
2018-10-21 13:18:02 +0900

    Add treeview

---
+++ TreeNodeTest/MainFOrm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TreeNodeTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filesTreeView = new System.Windows.Forms.TreeView();
+            this.contentListView = new System.Windows.Forms.ListView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // filesTreeView
+            // 
+            this.filesTreeView.Location = new System.Drawing.Point(0, 0);
+            this.filesTreeView.Name = "filesTreeView";
+            this.filesTreeView.Size = new System.Drawing.Size(366, 494);
+            this.filesTreeView.TabIndex = 0;
+            this.filesTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.filesTreeView_BeforeExpand);
+            this.filesTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.filesTreeView_NodeMouseClick);
+            // 
+            // contentListView
+            // 
+            this.contentListView.Location = new System.Drawing.Point(364, 0);
+            this.contentListView.Name = "contentListView";
+            this.contentListView.Size = new System.Drawing.Size(615, 494);
+            this.contentListView.TabIndex = 1;
+            this.contentListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(700, 511);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(799, 511);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(904, 511);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 546);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.contentListView);
+            this.Controls.Add(this.filesTreeView);
+            this.Name = "MainForm";
+            this.Text = "Explorer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView filesTreeView;
+        private System.Windows.Forms.ListView contentListView;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ TreeNodeTest/MainFOrm.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeNodeTest
+{
+    public partial class MainForm : Form
+    {
+        #region Constructor
+        public MainForm()
+        {
+            InitializeComponent();
+
+            foreach (var drive in Environment.GetLogicalDrives())
+            {
+                TreeNode treeNode = new TreeNode(drive);
+                treeNode.Nodes.Add(new TreeNode());
+                filesTreeView.Nodes.Add(treeNode);
+            }
+            initializeContentListView(Environment.GetLogicalDrives().First());
+        }
+        #endregion
+
+        #region Methods
+        private void initializeContentListView(string filePath)
+        {
+            this.contentListView.View = View.Details;
+            this.contentListView.Clear();
+            this.contentListView.Columns.Add("名前");
+            this.contentListView.Columns.Add("更新日時");
+            this.contentListView.Columns.Add("サイズ");
+
+            this.contentListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            try
+            {
+                if(Directory.Exists(filePath))
+                {
+                    //フォルダ一覧
+                    DirectoryInfo directoryList = new DirectoryInfo(filePath);
+
+                    foreach (var directory in directoryList.GetDirectories())
+                    {
+                        ListViewItem item = new ListViewItem(directory.Name);
+                        item.SubItems.Add(String.Format("{0:yyyy/MM/dd HH:mm:ss}", directory.LastAccessTime));
+                        item.SubItems.Add("");
+                        contentListView.Items.Add(item);
+                    }
+
+                    // ファイル一覧
+                    List<String> fileList = Directory.GetFiles(filePath).ToList<String>();
+
+                    foreach (var file in fileList)
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        ListViewItem item = new ListViewItem(fileInfo.Name);
+                        item.SubItems.Add(String.Format("{0:yyyy/MM/dd HH:mm:ss}", fileInfo.LastAccessTime));
+                        item.SubItems.Add(getFileSize(fileInfo.Length));
+                    }
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show(e.Message, "選択エラー");
+            }
+        }
+
+        /// <summary>
+        /// ファイルサイズを単位付きに変換して返す
+        /// </summary>
+        /// <param name="fileSize"></param>
+        /// <returns></returns>
+        private string getFileSize(long fileSize)
+        {
+            String ret = fileSize + " byte";
+            if (fileSize > (1024f * 1024f * 1024f))
+            {
+                ret = Math.Round((fileSize / 1024f / 1024f / 1024f), 2).ToString() + " GB";
+            }
+            else if (fileSize > (1024f * 1024f))
+            {
+                ret = Math.Round((fileSize / 1024f / 1024f), 2).ToString() + " MB";
+            }
+            else if (fileSize > 1024f)
+            {
+                ret = Math.Round((fileSize / 1024f)).ToString() + " KB";
+            }
+
+            return ret;
+        }
+        /// <summary>
+        /// ツリービューの指定されたディレクトリパスの項目を選択状態にします.
+        /// </summary>
+        /// <param name="dirPath"></param>
+        private void selectTreeViewItem(TreeView treeView, string dirPath)
+        {
+            // ツリービューのノードを走査
+            foreach (TreeNode node in treeView.Nodes)
+            {
+                selectTreeViewItem(node, dirPath);
+            }
+        }
+
+        /// <summary>
+        /// ツリービューの指定されたディレクトリパスの項目を選択状態にします（再帰処理用）.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="dirPath"></param>
+        private void selectTreeViewItem(TreeNode node, string dirPath)
+        {
+            foreach (TreeNode treeNode in node.Nodes)
+            {
+                FileInfo fi = new FileInfo(treeNode.FullPath);
+                // リストビューで表示されたディレクトリ名と一致する場合
+                if (dirPath == fi.FullName)
+                {
+                    this.filesTreeView.Focus();
+                    this.filesTreeView.SelectedNode = treeNode;
+                    break;
+                }
+                selectTreeViewItem(treeNode, dirPath);
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void filesTreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            initializeContentListView(e.Node.FullPath);
+        }
+
+        private void filesTreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            TreeNode childNode;
+            String path = node.FullPath;
+            node.Nodes.Clear();
+
+            try
+            {
+                if(Directory.Exists(path))
+                {
+                    DirectoryInfo directoryList = new DirectoryInfo(path);
+
+                    foreach (var directory in directoryList.GetDirectories())
+                    {
+                        childNode = new TreeNode(directory.Name);
+                        childNode.Nodes.Add(new TreeNode());
+                        node.Nodes.Add(childNode);
+                    }
+                    List<String> fileList = Directory.GetFiles(path).ToList();
+                    foreach (var fi in fileList)
+                    {
+                        childNode = new TreeNode(fi);
+                        childNode.Nodes.Add(new TreeNode());
+                        node.Nodes.Add(childNode);
+                    }
+                }             
+            }
+            catch (IOException ie)
+            {
+                MessageBox.Show(ie.Message, "選択エラー");
+            }
+        }
+        #endregion
+    }
+}
+
